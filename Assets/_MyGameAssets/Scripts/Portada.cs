6da3a7cbf526da20cc067e714e367e10c3648077
daher7 <daher7@gmail.com>
2018-10-29 13:52:43 +0100

    Niebla

---
+++ Assets/_MyGameAssets/Scripts/Portada.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portada : MonoBehaviour {
+
+    [SerializeField] RectTransform[] rts;
+    [SerializeField] float speed = 1f;
+
+    private void Update() { 
+        // Vamos a hacer que la niebla se mueva ciclicamente
+        for (int i = 0; i < rts.Length; i++) {
+            float xPos = -1 * Time.deltaTime * speed;
+            if (rts[i].position.x + rts[i].rect.width < 0) {
+                xPos = 1000;
+            }
+            rts[i].Translate(xPos, 0, 0);
+        }
+    }
+
+    public void StartGame() {
+        // Para cargar la escena principal
+        SceneManager.LoadScene(1);
+    }
+}
