1d76e2db596a5624658ff42cdf6733570aaccb44
daher7 <daher7@gmail.com>
2018-11-05 12:07:24 +0100

    Mosca

---
+++ Assets/_MyGameAssets/Scripts/Mosca.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mosca : MonoBehaviour {
+
+    bool haciaDerecha = false;
+    public Transform limiteDerecho;
+    public Transform limiteIzquierdo;
+
+    private void Start() {
+        // La mosca va estar inicialmente en el limite derecho
+        transform.position = limiteDerecho.position;
+    }
+
+    void Update () {
+
+        if (haciaDerecha) {
+            transform.Translate(Vector2.right * Time.deltaTime);
+            if(transform.position.x > limiteDerecho.position.x) {
+                haciaDerecha = false;
+                CambiarSentido();
+            }
+        } else {
+            transform.Translate(Vector2.left * Time.deltaTime);
+            // cuando llegue al limite izdo la mosca dara la vuelta
+            if(transform.position.x < limiteIzquierdo.position.x) {
+                haciaDerecha = true;
+                CambiarSentido();
+            }
+        }
+
+	}
+
+    void CambiarSentido() {
+        if (haciaDerecha) {
+            transform.localScale = new Vector2(-1, 1);
+        } else {
+            transform.localScale = new Vector2(1, 1);
+        }
+        
+    }
+}
--- Assets/_MyGameAssets/Scripts/Player.cs
+++ Assets/_MyGameAssets/Scripts/Player.cs
@@ -16,35 +16,37 @@ public class Player : MonoBehaviour {
     [SerializeField] int vidaMaxima = 100;
     [SerializeField] int puntos = 0;
     Rigidbody2D rb2D;
-    [SerializeField] int fuerzaPinchos; 
+    [SerializeField] int fuerzaPinchos;
     [SerializeField] Transform posPies;
     [SerializeField] float radioOverlap = 0.01f;
     public bool saltando = false;
     bool estoyLanzado = false;
 
-	void Start () {
-       
+    void Start() {
+
         rb2D = GetComponent<Rigidbody2D>();
         textPuntuacion.text = "Score: " + puntos.ToString();
-	}
+    }
 
     private void Update() {
-        xPos = Input.GetAxis("Horizontal");
-        if (saltando && Input.GetKeyDown(KeyCode.Space)) {
+        //xPos = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)) {
             rb2D.AddForce(new Vector3(0, jumpForce));
         }
     }
-
+    /*
     private void FixedUpdate()
     {
         saltando = Physics2D.OverlapCircle(posPies.position, radioOverlap, floorLayer);
+        print(saltando);
         rb2D.velocity = new Vector2(xPos * speed, rb2D.velocity.y);
     }
+    */
 
-    /*
-    void FixedUpdate () {   
+    void FixedUpdate() {
         float xPos = Input.GetAxis("Horizontal");
         float ySpeedActual = rb2D.velocity.y;
+
         if (!estoyLanzado) {
             // Vamos a comprobar si el personaje esta en el suelo o en el aire
             if (saltando) {
@@ -54,21 +56,48 @@ public class Player : MonoBehaviour {
                 } else {
                     rb2D.velocity = new Vector2(xPos * speed, ySpeedActual);
                 }
-            } else {
+            } else if ((Mathf.Abs(xPos) > 0.01f)) {
                 rb2D.velocity = new Vector2(xPos * speed, ySpeedActual);
             }
         }
-	}
+    }
 
     private bool EstaEnSuelo() {
         bool enSuelo = false;
         Collider2D col = Physics2D.OverlapCircle(posPies.position, radioOverlap, floorLayer);
-        if(col != null) {
+        if (col != null) {
             enSuelo = true;
         }
         return enSuelo;
     }
 
+
+    public void IncrementarPuntuacion(int puntosAIncrementar) {
+        puntos += puntosAIncrementar;
+        textPuntuacion.text = "Score: " + puntos.ToString();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        print(collision.gameObject.name);
+        //estoyLanzado = false;
+        if (collision.gameObject.CompareTag("Moneda")) {
+            IncrementarPuntuacion(1);
+            Destroy(collision.gameObject);
+        } else if (collision.gameObject.CompareTag("Pinchos")) {
+            QuitarVida(2);
+            print("Te has pinchado");
+        }
+
+    }
+
+    public void QuitarVida(int danyo) {
+        vidaActual -= danyo;
+        if (vidaActual <= 0) {
+            vidaActual = 0;
+        }
+        //estoyLanzado = true;
+        rb2D.AddRelativeForce(new Vector3(fuerzaPinchos * -1, fuerzaPinchos), ForceMode2D.Impulse);
+    }
     /*
      * Version basada en el TAG utilizando OverlapCircleAll
     private bool EstaEnSuelo() {
@@ -83,33 +112,5 @@ public class Player : MonoBehaviour {
         return enSuelo;
     }*/
 
-    public void IncrementarPuntuacion(int puntosAIncrementar) {
-        puntos += puntosAIncrementar;
-        textPuntuacion.text = "Score: " + puntos.ToString();
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision) {
-        print(collision.gameObject.name);
-        estoyLanzado = false;
-        if (collision.gameObject.CompareTag("Moneda")) { 
-            IncrementarPuntuacion(1);
-            Destroy(collision.gameObject);
-        }
-        else if (collision.gameObject.CompareTag("Pinchos")) {
-            QuitarVida(2);
-            print("Te has pinchado");
-        }
-
-    }
-    
-    public void QuitarVida(int danyo) {
-        vidaActual -= danyo;
-        if(vidaActual <= 0) {
-            vidaActual = 0;
-        }
-        estoyLanzado = true;
-        rb2D.AddRelativeForce(new Vector3(fuerzaPinchos*-1,fuerzaPinchos), ForceMode2D.Impulse);
-    }
-    
 }
-    
+
